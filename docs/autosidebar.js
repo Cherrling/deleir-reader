fe3b619ebde3dbcbcebf090f1f80db5d16405430
Cherrling <37233261+Cherrling@users.noreply.github.com>
2022-05-21 21:37:21 +0800

    Update autosidebar.js

--- docs/autosidebar.js
+++ docs/autosidebar.js
@@ -49,7 +49,7 @@ var fs = require('fs');
 
 
 console.log(sidebarTxt);
-fs.writeFile('./_sidebar.md', sidebarTxt,function(err){
+fs.writeFile('./docs/_sidebar.md', sidebarTxt,function(err){
     if(err){
         console.error(err);
     }
